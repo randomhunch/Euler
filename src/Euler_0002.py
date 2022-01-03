f4417756dd2317220d303b9f3969942d52f83b37
randomhunch <shawnbi052111@gmail.com>
2022-01-02 20:35:23 -0500

    Euler 2

--- src/Euler_0002.py
+++ src/Euler_0002.py
@@ -1,20 +1,10 @@
-A, B, C, D = 1, 2, 0, 0
+A, B ,C, sum = 1, 2, 0, 0
 
-while A < 4000000 and B < 4000000:
+while B < 4000000:
+
     if B % 2 == 0:
-        if D == 0:
-            A += B
-            D = 1
-            C += B
-        else:
-            B += A
-            D = 0
-            C += B
-        print(C)
-    else:
-        if D == 0:
-            A += B
-            D = 1
-        else:
-            B += A
-            D = 0
+        sum += B
+    C = A + B
+    A = B
+    B = C
+print(sum)
